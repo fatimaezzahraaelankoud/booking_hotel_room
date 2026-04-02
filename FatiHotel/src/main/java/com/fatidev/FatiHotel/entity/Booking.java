501965ee2e93a4f4f590f17c159ff5451aff2b13
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2026-04-03 00:23:58 +0200

    ajout room entity

--- FatiHotel/src/main/java/com/fatidev/FatiHotel/entity/Booking.java
+++ FatiHotel/src/main/java/com/fatidev/FatiHotel/entity/Booking.java
@@ -1,6 +1,9 @@
 package com.fatidev.FatiHotel.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -12,13 +15,19 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
-
+    @NotNull(message = "check in date is required")
     private LocalDate checkInDate ;
+
+    @Future(message="check out date must be in future")
     private LocalDate getCheckOutDate ;
 
+    @Min(value=1 ,message="number of adult must be more than 1")
     private int numOfAdults;
+    @Min(value=0 ,message="number of children  must  not be less than 0")
     private int numOfChildren ;
+
     private int numTotalOfGuest;
+
     private String bookingConfirmationCode;
 
     @ManyToOne(fetch = FetchType.EAGER)
@@ -28,4 +37,34 @@ public class Booking {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="room_id")
     private Room room ;
+
+    public void calculateTotaleOfGuest(){
+        this.numTotalOfGuest = this.numOfChildren+this.numOfAdults ;
+
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotaleOfGuest() ;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotaleOfGuest();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkInDate=" + checkInDate +
+                ", getCheckOutDate=" + getCheckOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", numTotalOfGuest=" + numTotalOfGuest +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                ", user=" + user +
+                ", room=" + room +
+                '}';
+    }
 }
--- FatiHotel/src/main/java/com/fatidev/FatiHotel/entity/Room.java
+++ FatiHotel/src/main/java/com/fatidev/FatiHotel/entity/Room.java
@@ -1,4 +1,36 @@
 package com.fatidev.FatiHotel.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="rooms")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl ;
+    private String roomDescription ;
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "Id=" + Id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                ", bookings=" + bookings +
+                '}';
+    }
 }
